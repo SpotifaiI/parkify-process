5460ab7e5f0991e5611dc11ea46a15f894980cb7
Cristian Prochnow <cristianprochnow@gmail.com>
2024-10-29 21:21:51 -0300

    feat: add migration for sense table

--- migrations/20241027194901_sense.ts
+++ migrations/20241027194901_sense.ts
@@ -1,8 +1,12 @@
-import type { Knex } from 'knex';
+import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('sense', schema => {
-    schema.increments('id').primary().unique().comment('Identificador único da captação do sensor.');
+    schema.increments('id').primary().unique().comment('Identificador único da captação do sensor');
+    schema.decimal('proximity', 8, 4).notNullable().comment('Leitura do sensor de proximidade');
+    schema.decimal('light', 8, 4).notNullable().comment('Leitura do fotorresistor');
+    schema.boolean('is_available').notNullable().comment('Indica se a vaga está sendo ocupada ou não conforme a lógica embarcada');
+    schema.datetime('created_at').defaultTo(knex.fn.now()).comment('Momento que ocorreu a gravação');
   });
 }
 
